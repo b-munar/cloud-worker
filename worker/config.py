da6978cd89e3d61bced35221992a459494e09553
mathbrahian <mathbrahian@gmail.com>
2023-04-21 09:45:09 -0500

    first commit

---
+++ worker/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    USERNAME=os.getenv("USERNAME")
+    HOST=os.getenv("HOST")
+    DATABASE=os.getenv("DATABASE")
+    PASSWORD=os.getenv("PASSWORD")
+    PORT=os.getenv("PORT")
---
+++ worker/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p /home/celery/var/run/
+
+poetry run celery -A tasks worker -B -s /home/celery/var/run/celerybeat-schedule
