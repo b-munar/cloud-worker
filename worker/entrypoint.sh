f543e0e65ec8f9c7b99e40b2effdc685a2ce8ce2
b-munar <b.munar@uniandes.edu.co>
2023-05-06 03:24:16 -0500

    trabajando normal

--- worker/entrypoint.sh
+++ worker/entrypoint.sh
@@ -2,5 +2,5 @@
 
 mkdir -p /home/celery/var/run/
 
-# poetry run celery -A tasks worker -B -s /home/celery/var/run/celerybeat-schedule
-poetry run celery -A tasks worker
+poetry run celery -A tasks worker -B -s /home/celery/var/run/celerybeat-schedule
+# poetry run celery -A tasks worker
