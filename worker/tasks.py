df3e355d3af778e68f84727883dfcfba7451da68
b-munar <b.munar@uniandes.edu.co>
2023-05-06 03:19:14 -0500

    colocando limit al lote

--- worker/tasks.py
+++ worker/tasks.py
@@ -63,12 +63,12 @@ def compress_tarbz2(file_id):
 @app.task()
 def queueing():
     session = Session()
-    for task_to_zip in session.query(Task).filter(Task.status == False, Task.type_task==1):
+    for task_to_zip in session.query(Task).filter(Task.status == False, Task.type_task==1).limit(10):
         compress_zip.delay(task_to_zip.file_id)
     for task_to_targz in  session.query(Task).filter(Task.status == False, Task.type_task==2):
         compress_targz.delay(task_to_targz.file_id)
     for task_to_tarbz2 in  session.query(Task).filter(Task.status == False, Task.type_task==3):
         compress_tarbz2.delay(task_to_tarbz2.file_id)
-    #session.close()
+    session.close()
         
         
