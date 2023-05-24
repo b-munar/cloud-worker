f40de6586e80459ec9be7b1e3ccfb823daffb363
b-munar <b.munar@uniandes.edu.co>
2023-05-24 14:20:20 -0500

    se volvio a la estrategia de comprimir los archivos en memoria ram

--- worker/utils/compress.py
+++ worker/utils/compress.py
@@ -4,7 +4,7 @@ from cloud_db.models import File, Task
 from database import Session
 from utils.gcp_storage import download_file, upload_file
 import os
-import shutil
+from io import BytesIO
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = '/usr/src/app/linen-mason-384315-d051f5f132d3.json'
 
@@ -13,21 +13,22 @@ def compress_zip(file_id):
     session = Session()
     file_to_zip = session.query(File).filter(File.id == file_id).first() 
 
-    os.makedirs(f"/{file_to_zip.dir}", exist_ok=True)
+    file_download, content_type = download_file(bucket_name="gropo-2-nube-2023", file_name=file_to_zip.path)
 
-    download_file(bucket_name="gropo-2-nube-2023", file_cloud_name=file_to_zip.path)
+    in_memory_zip = BytesIO()
+
+    with zipfile.ZipFile(in_memory_zip, "a", zipfile.ZIP_DEFLATED, False) as zip:
+        zip.writestr(file_to_zip.name, file_download)
+
+    in_memory_zip.seek(0)
     
-    with zipfile.ZipFile(f"/{file_to_zip.dir}/{file_to_zip.name.split('.')[0]}.zip", 'w') as zip:
-        zip.write(f"/{file_to_zip.path}")
-
-
     upload_file(bucket_name="gropo-2-nube-2023", 
-                source_file=f"/{file_to_zip.dir}/{file_to_zip.name.split('.')[0]}.zip", 
+                source_file=in_memory_zip, 
                 destination_file_name=f"{file_to_zip.dir}/{file_to_zip.name.split('.')[0]}.zip",
                 content_type="application/zip"
                 )
 
-    shutil.rmtree(f"/{file_to_zip.dir}")
+    in_memory_zip.close()
     
     task = session.query(Task).filter(Task.file_id==file_id).first()
     task.status=True
